546f10e9b3225b5d4dbef6edfc04ca268e4318a9
hor00s <hor00s199@gmail.com>
2023-09-25 11:04:00 +0300

    Updated tests for bot

--- traffic-bot/bot/tests.py
+++ traffic-bot/bot/tests.py
@@ -10,38 +10,32 @@ class TestBot(unittest.TestCase):
     def tearDown(self) -> None:
         return super().tearDown()
 
+    def check_result_24f(self, time: int) -> None:
+        bot = Bot("", 24)
+        keys = list(bot.data.keys())
+        _time = dt.datetime(year=1970, month=1, day=1, hour=time)
+        key = bot._find_index(_time)
+        value = keys[key]
+        time_to_str = f"{time}:00" if len(str(time)) == 2 else f"0{time}:00"        
+        self.assertEqual(value, time_to_str)
+
+    def check_result_12f(self, time: int) -> None:
+        bot = Bot("", 12)
+        keys = list(bot.data.keys())
+        _time = dt.datetime(year=1970, month=1, day=1, hour=time)
+        key = bot._find_index(_time)
+        value = keys[key]
+        if time < 12:
+            mid = 'AM'
+        else:
+            mid = 'PM'
+            time = time - 12
+        time_to_str = f"{time}:00 {mid}" if len(str(time)) == 2 else f"0{time}:00 {mid}"        
+        self.assertEqual(value, time_to_str)
+
     def test_get_time(self) -> None:
-        bot = Bot("", 24)
-        keys = list(bot.data.keys())
-        time = dt.datetime(year=1970, month=1, day=1, hour=5)
-        key = bot._find_index(time)
-        value = keys[key]
-        self.assertEqual(value, "05:00")
+        for i in range(24):
+            self.check_result_24f(i)
 
-        bot = Bot("", 24)
-        keys = list(bot.data.keys())
-        time = dt.datetime(year=1970, month=1, day=1, hour=12)
-        key = bot._find_index(time)
-        value = keys[key]
-        self.assertEqual(value, "12:00")
-
-        bot = Bot("", 24)
-        keys = list(bot.data.keys())
-        time = dt.datetime(year=1970, month=1, day=1, hour=0)
-        key = bot._find_index(time)
-        value = keys[key]
-        self.assertEqual(value, "00:00")
-
-        bot = Bot("", 12)
-        keys = list(bot.data.keys())
-        time = dt.datetime(year=1970, month=1, day=1, hour=5)
-        key = bot._find_index(time)
-        value = keys[key]
-        self.assertEqual(value, "05:00 AM")
-
-        bot = Bot("", 12)
-        keys = list(bot.data.keys())
-        time = dt.datetime(year=1970, month=1, day=1, hour=18)
-        key = bot._find_index(time)
-        value = keys[key]
-        self.assertEqual(value, "06:00 PM")
+        for i in range(1, 11):
+            self.check_result_12f(i)
